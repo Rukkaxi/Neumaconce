781bd3708359869b2cde875a699fd90d653f927d
FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
2024-05-15 12:44:08 -0400

    migration

--- database/migrations/2024_05_12_142940_create_permission_tables.php
+++ database/migrations/2024_05_12_142940_create_permission_tables.php
@@ -34,7 +34,7 @@ class CreatePermissionTables extends Migration
             $table->unique(['name', 'guard_name']);
         });
 
-        Schema::create('payment_methods', function (Blueprint $table) {
+        Schema::create($tableNames['payment_methods'], function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');       // For MySQL 8.0 use string('name', 125);
             $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
