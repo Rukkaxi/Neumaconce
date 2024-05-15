529aeac86a9178311fdfa480d0be1df878e327e6
FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
2024-05-15 19:43:09 -0400

    Update PaymentMethodController.php

--- app/Http/Controllers/PaymentMethodController.php
+++ app/Http/Controllers/PaymentMethodController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\PaymentMethod;
+use Illuminate\Support\Facades\Storage;
 
 class PaymentMethodController extends Controller
 {
