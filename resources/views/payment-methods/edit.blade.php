3f0c061d702244ac434660725029212ff2a9ffe8
FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
2024-05-14 23:41:22 -0400

    Update edit.blade.php

--- resources/views/payment-methods/edit.blade.php
+++ resources/views/payment-methods/edit.blade.php
@@ -1,23 +1,40 @@
 @extends('layouts.backend')
 
 @section('content')
-    <h1>Editar Método de Pago</h1>
-    <form action="{{ route('payment-methods.update', $paymentMethod->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
-        <div class="form-group">
-            <label for="name">Nombre</label>
-            <input type="text" id="name" name="name" value="{{ $paymentMethod->nombre }}" required>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Editar Método de Pago
+                        <a href=" {{url('payment-methods')}} " class="btn btn-success float-end">Volver</a>
+                    </h4>
+                </div>
+
+                <div class="card-body">
+                    <form action="{{ route('payment-methods.update', $paymentMethod->id) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">>
+                            <label for="name">Nombre</label>
+                            <input type="text" name="name" value="{{ $paymentMethod->name }}" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="description">Descripción</label>
+                            <input type="text" name="description" value="{{ $paymentMethod->description }}" class="form-control" required >
+                        </div>
+                        <div class="mb-3">
+                            <label for="photo">Foto</label>
+                            <input type="file" name="photo" class="form-control" id="photo">
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-success">Actualizar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="description">Descripción</label>
-            <input type="text" id="description" name="description" value="{{ $paymentMethod->descripcion }}" required>
-        </div>
-        <div class="form-group">
-            <label for="photo">Foto</label>
-            <input type="file" name="photo" class="form-control" id="photo">
-        </div>
-        <button type="submit" class="btn btn-primary">Actualizar</button>
-        
-    </form>
+    </div>
+</div>
 @endsection
