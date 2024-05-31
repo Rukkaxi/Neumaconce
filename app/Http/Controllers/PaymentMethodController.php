8b3bfb0d8c21e46a5e9a7a357e7fe7f89bde5c4e
FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
2024-05-30 23:46:39 -0400

    Squashed commit of the following:
    commit a4abbb4790b08272ca20bdfc87ed8daa228ffc83
    Merge: d3854db c2c314e
    Author: FAMellado <128266640+FAMellado@users.noreply.github.com>
    Date:   Thu May 30 21:27:25 2024 -0400
    
        Merge branch 'fArrans' into DEV
    
    commit d3854db95175a2abee4c6c2615161aa2bb4d4529
    Merge: 76cb281 dd65d1b
    Author: FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
    Date:   Wed May 15 22:29:37 2024 -0400
    
        Merge branch 'fAviles' into DEV
    
    commit dd65d1bb8b9e72580fbb3d7ae89315fb5dc0714d
    Author: FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
    Date:   Wed May 15 22:23:49 2024 -0400
    
        Update .env
    
    commit 9239f82a5aef1757d6e42922c35649740e562953
    Author: FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
    Date:   Wed May 15 22:23:38 2024 -0400
    
        migracion metodo de pago
    
    commit 3c89e3c2a086c7eeab9fc8a2d9fc176c1950209f
    Author: FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
    Date:   Wed May 15 20:41:05 2024 -0400
    
        .
    
    commit 76cb281ca235a326440015d90f31d2683eddc030
    Author: FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
    Date:   Wed May 15 19:43:09 2024 -0400
    
        Update PaymentMethodController.php
    
    commit d8b97a03b999e34b397fcd39ff73c0cf26bceebb
    Author: FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
    Date:   Wed May 15 19:42:33 2024 -0400
    
        Update edit.blade.php
    
    commit 4cd8777ad3c8e505602a5bce29de4769e188e0ca
    Author: FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
    Date:   Wed May 15 19:35:53 2024 -0400
    
        storage

--- app/Http/Controllers/PaymentMethodController.php
+++ app/Http/Controllers/PaymentMethodController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\PaymentMethod;
 use Illuminate\Support\Facades\Storage;
 
+
 class PaymentMethodController extends Controller
 {
     public function index()
