a75486422863f3791c651564a5a73768ed5d1775
FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
2024-06-10 16:29:47 -0400

    Update preorder.blade.php

--- resources/views/cart/preorder.blade.php
+++ resources/views/cart/preorder.blade.php
@@ -105,84 +105,83 @@
                             </div>
                             <div class="form-group">
                                 <label>Identifica a la persona que retira</label><br>
-                                <input type="radio" id="buyer_pickup" name="pickup_person" value="buyer" onclick="togglePickupPersonName()">
+                                <input type="radio" id="buyer_pickup" name="pickup_person" value="buyer" onclick="togglePickupPerson()">
                                 <label for="buyer_pickup">La misma persona que compra</label><br>
-                                <input type="radio" id="other_pickup" name="pickup_person" value="other" onclick="togglePickupPersonName()">
-                                <label for="other_pickup">Otra persona retirará</label><br>
-                                <input type="text" id="pickup_person_name" name="pickup_person_name" class="form-control" style="display: none;" placeholder="Nombre de la persona que retira">
+                                <input type="radio" id="other_pickup" name="pickup_person" value="other" onclick="togglePickupPerson()">
+                                <label for="other_pickup">Otra persona retirará</label>
+                            </div>
+                            <div id="other-pickup-name" class="form-group" style="display: none;">
+                                <label for="pickup_name">Nombre de la persona que retira</label>
+                                <input type="text" id="pickup_name" name="pickup_name" class="form-control">
                             </div>
                         </div>
                         
                         <div id="home-delivery-options" style="display: none;">
                             <div class="form-group">
-                                <label>Direcciones de despacho</label><br>
-                                <input type="radio" id="customer_address" name="delivery_address" value="customer_address">
-                                <label for="customer_address">Direcciones cliente</label>
-                                <a href="javascript:void(0);" onclick="showNewAddressForm()" style="margin-left: 10px;">Añadir nueva dirección</a>
+                                <label for="address">Direcciones de despacho</label><br>
+                                        <div class="form-group">
+                                            <select id="address" name="address" class="form-control">
+                                                <option value="" disabled selected>Seleccione una dirección</option>
+                                                @foreach($addresses as $address)
+                                                    <option value="{{ $address->id }}">{{ $address->full_address }}</option>
+                                                @endforeach
+                                            </select>
+                                            <a href="javascript:void(0);" onclick="showNewAddressForm()">Añadir dirección</a>
+                                        </div>
+                                        
+                                        <div id="new-address-form" style="display: none; margin-top: 15px;">
+                                            <h3>Nueva Dirección</h3>
+                                            <div class="mb-3">
+                                                <label for="address1">Calle</label>
+                                                <input type="text" id="address1" name="address1" class="form-control">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="number">Número</label>
+                                                <input type="text" id="number" name="number" class="form-control">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="address2">Indicaciones (Opcional)</label>
+                                                <input type="text" id="address2" name="address2" class="form-control">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="commune_id">Comuna</label>
+                                                <select id="commune_id" name="commune_id" class="form-control">
+                                                    <option value="" disabled selected>Seleccione una comuna</option>
+                                                    @foreach($communes as $commune)
+                                                        <option value="{{ $commune->id }}">{{ $commune->name }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <button type="button" class="btn btn-primary" onclick="saveAddress()">Guardar Dirección</button>
+                                        </div>
                             </div>
                             <div class="form-group">
                                 <label>Identifica a la persona que recibe</label><br>
-                                <input type="radio" id="buyer_receive" name="receive_person" value="buyer" onclick="toggleReceivePersonName()">
+                                <input type="radio" id="buyer_receive" name="receive_person" value="buyer" onclick="toggleReceivePerson()">
                                 <label for="buyer_receive">La misma persona que compra</label><br>
-                                <input type="radio" id="other_receive" name="receive_person" value="other" onclick="toggleReceivePersonName()">
+                                <input type="radio" id="other_receive" name="receive_person" value="other" onclick="toggleReceivePerson()">
                                 <label for="other_receive">Otra persona recibirá</label><br>
-                                <input type="text" id="receive_person_name" name="receive_person_name" class="form-control" style="display: none;" placeholder="Nombre de la persona que recibe">
-                                <input type="radio" id="concierge" name="receive_person" value="concierge" onclick="toggleReceivePersonName()">
+                                <input type="radio" id="concierge" name="receive_person" value="concierge" onclick="toggleReceivePerson()">
                                 <label for="concierge">Dejar en conserjería</label>
                             </div>
+                            <div id="other-receive-name" class="form-group" style="display: none;">
+                                <label for="receive_name">Nombre de la persona que recibirá</label>
+                                <input type="text" id="receive_name" name="receive_name" class="form-control">
+                            </div>
+                            
                         </div>
                         
+                        
+                        
                         <div class="form-group">
-                            <label for="address">Dirección</label>
-                            <select id="address" name="address" class="form-control">
-                                <option value="" disabled selected>Seleccione una dirección</option>
-                                @foreach($addresses as $address)
-                                    <option value="{{ $address->id }}">{{ $address->full_address }}</option>
+                            <label for="payment_method">Método de pago</label>
+                            <select id="payment_method" name="payment_method" class="form-control">
+                                @foreach($paymentMethods as $method)
+                                    <option value="{{ $method->id }}">{{ $method->name }}</option>
                                 @endforeach
                             </select>
-                            <a href="javascript:void(0);" onclick="showNewAddressForm()">Añadir dirección</a>
                         </div>
-                        
-                        <div id="new-address-form" style="display: none; margin-top: 15px;">
-                            <h3>Nueva Dirección</h3>
-                            <div class="mb-3">
-                                <label for="address1">Calle</label>
-                                <input type="text" id="address1" name="address1" class="form-control">
-                            </div>
-                            <div class="mb-3">
-                                <label for="number">Número</label>
-                                <input type="text" id="number" name="number" class="form-control">
-                            </div>
-                            <div class="mb-3">
-                                <label for="address2">Indicaciones (Opcional)</label>
-                                <input type="text" id="address2" name="address2" class="form-control">
-                            </div>
-                            <div class="mb-3">
-                                <label for="commune_id">Comuna</label>
-                                <select id="commune_id" name="commune_id" class="form-control">
-                                    <option value="" disabled selected>Seleccione una comuna</option>
-                                    @foreach($communes as $commune)
-                                        <option value="{{ $commune->id }}">{{ $commune->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <button type="button" class="btn btn-primary" onclick="saveAddress()">Guardar Dirección</button>
-                        </div>
-                        
-                        <div class="form-group">
-                            <label for="phone">Teléfono para coordinar la entrega</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <select id="country_code" name="country_code" class="form-control">
-                                        <option value="+56">+56</option>
-                                        <!-- Agregar más opciones según sea necesario -->
-                                    </select>
-                                </div>
-                                <input type="text" id="phone" name="phone" class="form-control" placeholder="Teléfono">
-                            </div>
-                        </div>
-                        
-                        <button type="submit" class="btn btn-primary">Continuar</button>
+                        <button type="submit" class="btn btn-primary">Comprar</button>
                     </form>
                 </div>
             </div>
@@ -191,45 +190,48 @@
 </div>
 
 <script>
-function toggleDeliveryOptions() {
-    var storePickupOptions = document.getElementById('store-pickup-options');
-    var homeDeliveryOptions = document.getElementById('home-delivery-options');
-    
-    if (document.getElementById('store_pickup').checked) {
-        storePickupOptions.style.display = 'block';
-        homeDeliveryOptions.style.display = 'none';
-    } else if (document.getElementById('home_delivery').checked) {
-        storePickupOptions.style.display = 'none';
-        homeDeliveryOptions.style.display = 'block';
+    function showNewAddressForm() {
+        document.getElementById('new-address-form').style.display = 'block';
     }
-}
 
-function togglePickupPersonName() {
-    var pickupPersonName = document.getElementById('pickup_person_name');
-    if (document.getElementById('other_pickup').checked) {
-        pickupPersonName.style.display = 'block';
-    } else {
-        pickupPersonName.style.display = 'none';
+    function saveAddress() {
+        var address1 = document.getElementById('address1').value;
+        var number = document.getElementById('number').value;
+        var address2 = document.getElementById('address2').value;
+        var commune_id = document.getElementById('commune_id').value;
+
+        // Validar y enviar los datos al servidor para guardar la nueva dirección
     }
-}
 
-function toggleReceivePersonName() {
-    var receivePersonName = document.getElementById('receive_person_name');
-    if (document.getElementById('other_receive').checked) {
-        receivePersonName.style.display = 'block';
-    } else {
-        receivePersonName.style.display = 'none';
+    function toggleDeliveryOptions() {
+        var storePickup = document.getElementById('store_pickup').checked;
+        var homeDelivery = document.getElementById('home_delivery').checked;
+        
+        if (storePickup) {
+            document.getElementById('store-pickup-options').style.display = 'block';
+            document.getElementById('home-delivery-options').style.display = 'none';
+        } else if (homeDelivery) {
+            document.getElementById('store-pickup-options').style.display = 'none';
+            document.getElementById('home-delivery-options').style.display = 'block';
+        }
     }
-}
 
-function showNewAddressForm() {
-    var newAddressForm = document.getElementById('new-address-form');
-    newAddressForm.style.display = 'block';
-}
+    function togglePickupPerson() {
+        var otherPickup = document.getElementById('other_pickup').checked;
+        if (otherPickup) {
+            document.getElementById('other-pickup-name').style.display = 'block';
+        } else {
+            document.getElementById('other-pickup-name').style.display = 'none';
+        }
+    }
 
-function saveAddress() {
-    // Lógica para guardar la nueva dirección
-    alert('Nueva dirección guardada');
-}
+    function toggleReceivePerson() {
+        var otherReceive = document.getElementById('other_receive').checked;
+        if (otherReceive) {
+            document.getElementById('other-receive-name').style.display = 'block';
+        } else {
+            document.getElementById('other-receive-name').style.display = 'none';
+        }
+    }
 </script>
 @endsection
