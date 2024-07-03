57534a4eb25247c44611410de5085a41b556bbdf
Rukkaxi <43216245+Rukkaxi@users.noreply.github.com>
2024-07-03 04:50:36 -0400

    Gráficos y funciones de los mismos
    Se crearon 4 gráficos:
    1 top categorias más vistas
    2 top productos mas vendidos
    historicos de ventas diarias y cantidad de productos vendidos

--- resources/views/graphics/index.blade.php
+++ resources/views/graphics/index.blade.php
@@ -9,7 +9,13 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-6">
-                            <div id="container" style="height: 400px;"></div>
+                            <div id="ventas" style="height: 400px;"></div>
+                        </div>
+                        <div class="col-md-6">
+                            <div id="categoriaChart" style="height: 400px;"></div>
+                        </div>
+                        <div class="col-md-6">
+                            <div id="ventas_2" style="height: 400px;"></div>
                         </div>
                         <div class="col-md-6">
                             <div id="pieChart" style="height: 400px;"></div>
@@ -28,31 +34,104 @@
 <script src="https://code.highcharts.com/modules/accessibility.js"></script>
 
 <script type="text/javascript">
-    // Convertimos las variables PHP a variables JavaScript
-    var salesData = @json($data);
+    // Función de animación personalizada
+    (function(H) {
+        H.seriesTypes.pie.prototype.animate = function(init) {
+            const series = this,
+                chart = series.chart,
+                points = series.points,
+                {
+                    animation
+                } = series.options,
+                {
+                    startAngleRad
+                } = series;
 
-    // Preparar datos para el gráfico
-    var categories = salesData.map(item => item.sale_date);
-    var salesAmounts = salesData.map(item => parseFloat(item.total_sales));
+            function fanAnimate(point, startAngleRad) {
+                const graphic = point.graphic,
+                    args = point.shapeArgs;
 
-    // Convertir fechas a objetos Date y filtrar últimos 7 días
-    var today = new Date();
-    today.setHours(0, 0, 0, 0); // Ajustar a la medianoche
-    var last7Days = salesData.filter(item => {
-        var saleDate = new Date(item.sale_date);
-        return saleDate >= new Date(today.getTime() - 6 * 24 * 60 * 60 * 1000); // últimos 7 días
+                if (graphic && args) {
+
+                    graphic
+                        .attr({
+                            start: startAngleRad,
+                            end: startAngleRad,
+                            opacity: 1
+                        })
+                        .animate({
+                            start: args.start,
+                            end: args.end
+                        }, {
+                            duration: animation.duration / points.length
+                        }, function() {
+                            if (points[point.index + 1]) {
+                                fanAnimate(points[point.index + 1], args.end);
+                            }
+                            if (point.index === series.points.length - 1) {
+                                series.dataLabelsGroup.animate({
+                                        opacity: 1
+                                    },
+                                    void 0,
+                                    function() {
+                                        points.forEach(point => {
+                                            point.opacity = 1;
+                                        });
+                                        series.update({
+                                            enableMouseTracking: true
+                                        }, false);
+                                        chart.update({
+                                            plotOptions: {
+                                                pie: {
+                                                    innerSize: '40%',
+                                                    borderRadius: 8
+                                                }
+                                            }
+                                        });
+                                    });
+                            }
+                        });
+                }
+            }
+
+            if (init) {
+                points.forEach(point => {
+                    point.opacity = 0;
+                });
+            } else {
+                fanAnimate(points[0], startAngleRad);
+            }
+        };
+    }(Highcharts));
+
+    Highcharts.setOptions({
+        colors: ['#3c90df', '#3682c4', '#3174a9', '#2c668e', '#275973']
     });
 
-    // Actualizar categorías y montos con los últimos 7 días
+    // Datos de ventas
+    var salesData = @json($data);
+
+    var categories = salesData.map(item => item.sale_date);
+    var salesAmounts = salesData.map(item => parseFloat(item.total_sales));
+    var totalQuantities = salesData.map(item => parseInt(item.total_quantity));
+
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
+    var last7Days = salesData.filter(item => {
+        var saleDate = new Date(item.sale_date);
+        return saleDate >= new Date(today.getTime() - 6 * 24 * 60 * 60 * 1000);
+    });
+
     categories = last7Days.map(item => item.sale_date);
     salesAmounts = last7Days.map(item => parseFloat(item.total_sales));
+    totalQuantities = last7Days.map(item => parseInt(item.total_quantity));
 
-    Highcharts.chart('container', {
+    Highcharts.chart('ventas', {
         chart: {
-            type: 'line' 
+            type: 'line'
         },
         title: {
-            text: 'Historia de compras'
+            text: 'Historico De Compras'
         },
         xAxis: {
             categories: categories,
@@ -75,30 +154,110 @@
         }]
     });
 
+    var topCategories = @json($topCategories);
+
+    var pieData = Object.keys(topCategories).map(function(key) {
+        return {
+            name: key,
+            y: topCategories[key]
+        };
+    });
+
+    Highcharts.chart('categoriaChart', {
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: 'Distribución De Ventas Por Categoría'
+        },
+        series: [{
+            name: 'Ventas',
+            enableMouseTracking: false, // Deshabilitar seguimiento del mouse al cargar
+            animation: {
+                duration: 2000
+            },
+            colorByPoint: true,
+            data: pieData
+        }],
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                borderWidth: 2,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>',
+                    distance: 20
+                }
+            }
+        }
+    });
+
+    Highcharts.chart('ventas_2', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Historico de Productos Vendidos'
+        },
+        xAxis: {
+            categories: categories,
+            title: {
+                text: 'Fecha de ventas'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Cantidad vendida'
+            }
+        },
+        series: [{
+            name: 'Cantidad vendida',
+            data: totalQuantities,
+            colorByPoint: false,
+            tooltip: {
+                valueSuffix: ' unidades'
+            }
+        }]
+    });
+
+    var topProducts = @json($topProducts);
+    var productNames = Object.keys(topProducts);
+    var productQuantities = Object.values(topProducts);
+
     Highcharts.chart('pieChart', {
         chart: {
             type: 'pie'
         },
         title: {
-            text: 'Distribución de ventas por categoría'
+            text: 'Top 10 Productos Más Vendidos'
         },
         series: [{
             name: 'Ventas',
+            enableMouseTracking: false, // Deshabilitar seguimiento del mouse al cargar
+            animation: {
+                duration: 2000
+            },
             colorByPoint: true,
-            data: [{
-                    name: 'Categoría 1',
-                    y: 30
-                },
-                {
-                    name: 'Categoría 2',
-                    y: 20
-                },
-                {
-                    name: 'Categoría 3',
-                    y: 50
+            data: productNames.map((name, index) => {
+                return {
+                    name: name,
+                    y: productQuantities[index]
+                };
+            })
+        }],
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                borderWidth: 2,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.y}',
+                    distance: 20
                 }
-            ]
-        }]
+            }
+        }
     });
 </script>
 @endsection
